11bcbbdce32803fab00d76b522935113c2043431
taewoocode <qkrxodn6035@gmail.com>
2024-07-01 21:31:31 +0900

    hash

---
+++ src/collection/set/HashStart1.java
@@ -0,0 +1,22 @@
+package collection.set;
+
+import java.util.Arrays;
+
+public class HashStart1 {
+    public static void main(String[] args) {
+        Integer[] inputArray = new Integer[4];
+        inputArray[0] = 1;
+        inputArray[1] = 2;
+        inputArray[2] = 5;
+        inputArray[3] = 8;
+        System.out.println( "inputArray = " + Arrays.toString( inputArray ) );
+
+        int searchValue = 8;
+        //O(n) search
+        for (Integer inputValue : inputArray) {
+            if (inputValue == searchValue) {
+                System.out.println( "inputValue = " + inputValue );
+            }
+        }
+    }
+}
