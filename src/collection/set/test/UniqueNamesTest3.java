3c97643db0ea3bdd7d0c7a8ddbf8d7444c85f4e8
taewoocode <qkrxodn6035@gmail.com>
2024-07-03 17:31:53 +0900

    test3

---
+++ src/collection/set/test/UniqueNamesTest3.java
@@ -0,0 +1,17 @@
+package collection.set.test;
+
+import java.util.LinkedHashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.TreeSet;
+
+public class UniqueNamesTest3 {
+    public static void main(String[] args) {
+        Set<Integer> set = new TreeSet<>( List.of( 30, 20, 20, 10, 10 ) ); //입력한 순서를 보장하는 LinkedHashSet
+
+        for (Integer s : set) {
+            System.out.println( s );
+        }
+
+    }
+}
